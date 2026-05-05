36de336c1465ba46c167964a00f1ac1bf6ba0acb
shivathegreat724 <vg.shiva14@gmail.com>
2026-05-05 17:57:06 +0100

    Add files via upload

---
+++ uppercase.py
@@ -0,0 +1,10 @@
+class IOString:
+    def __init__(self):
+        self.str1 = ""
+    def get_String(self):
+        self.str1 = input("Enter a string")
+    def print_String(self):
+        print(self.str1.upper())
+str1 = IOString()
+str1.get_String()
+str1.print_String()       
