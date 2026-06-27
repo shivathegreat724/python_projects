e13c3ca9ca33d9f6b35bc5064317a7233e70225d
shivathegreat724 <vg.shiva14@gmail.com>
2026-06-27 12:16:20 +0100

    Add files via upload

---
+++ windows4.py
@@ -0,0 +1,13 @@
+from tkinter import *
+window =Tk()
+window.title("Event Handler")
+window.geometry("100x100")
+def handle_key_press(event):
+    print(event.char)
+window.bind("<Key>", handle_key_press)
+def handle_click(event):
+    print("\nThe button was clicked.")
+button = Button(text = "Click me!")
+button.pack()
+button.bind("<Button-1>, handle_click")
+window.mainloop()
